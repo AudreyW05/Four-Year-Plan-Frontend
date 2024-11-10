d2f42980a29196f7330ab3088c66f26ed5602448
AudreyW05 <78786071+AudreyW05@users.noreply.github.com>
2024-11-09 16:58:42 -0800

    modify sidebar styling

--- src/components/Home/HomeSidebar/HomeSidebar.tsx
+++ src/components/Home/HomeSidebar/HomeSidebar.tsx
@@ -1,77 +1,48 @@
 import React, { useState } from 'react';
 import ClassCategories from './ClassCategories/ClassCategories';
-import { 
-    Box, 
-    Drawer,
-    CssBaseline,
-    IconButton,
-} from '@mui/material';
-import { styled, useTheme } from '@mui/material/styles';
+import { Box, Drawer, IconButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 
 const HomeSidebar = () => {
-    const [open, setOpen] = useState(false);
-    
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-    
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
+  const [open, setOpen] = useState(false);
 
-    return (
-        <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
-            <IconButton
-                    aria-label="open drawer"
-                    onClick={handleDrawerOpen}
-                    edge="start"
-                    sx={[
-                    {
-                        mr: 2,
-                        ml: 2,
-                        mt: 1,
-                    },
-                    open && { display: 'none' },
-                    ]}
-                >
-                <MenuIcon />
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box className='flex mt-24'>
+      <IconButton
+        aria-label='open drawer'
+        onClick={handleDrawerOpen}
+        edge='start'
+        className='mr-2 ml-4 mt-1'
+        sx={[open && { display: 'none' }]}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Drawer variant='persistent' open={open}>
+        <Box className='flex-col w-64'>
+          <Box className='flex items-center px-2 my-4 justify-end'>
+            <IconButton onClick={handleDrawerClose}>
+              <ChevronLeftIcon />
             </IconButton>
-            <Drawer variant='persistent' 
-            sx={{ width: 277,
-                '& .MuiDrawer-paper': {
-                    width: 277,
-                    flexShrink: 0,
-                    backgroundColor: '#f1f8fd',
-                },
-                }} open={open}>
-
-                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            padding: (theme) => theme.spacing(0, 1),
-                            ...((theme) => theme.mixins.toolbar),
-                            justifyContent: 'flex-end',
-                        }}
-                    >
-                        <IconButton onClick={handleDrawerClose}>
-                            <ChevronLeftIcon />
-                        </IconButton>
-                    </Box>
-                    <ClassCategories sectname={'Lower Division'}></ClassCategories>
-                    <ClassCategories sectname={'Upper Division'}></ClassCategories>
-                    <ClassCategories sectname={'Math'}></ClassCategories>
-                    <ClassCategories sectname={'Physics'}></ClassCategories>
-                    <ClassCategories sectname={'GEs'}></ClassCategories>
-                    <ClassCategories sectname={'Others'}></ClassCategories>
-                </Box>
-            </Drawer>  
-        </Box>  
-    )
-}
+          </Box>
+          <ClassCategories sectname={'Lower Division'}></ClassCategories>
+          <ClassCategories sectname={'Upper Division'}></ClassCategories>
+          <ClassCategories sectname={'Math'}></ClassCategories>
+          <ClassCategories sectname={'Physics'}></ClassCategories>
+          <ClassCategories sectname={'GEs'}></ClassCategories>
+          <ClassCategories sectname={'Others'}></ClassCategories>
+        </Box>
+      </Drawer>
+    </Box>
+  );
+};
 
 export default HomeSidebar;
