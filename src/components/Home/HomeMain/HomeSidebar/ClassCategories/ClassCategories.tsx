30005222825f31705e4c9d70ce49499fc4630a4c
Travis Ha <travisha33@gmail.com>
2024-11-24 15:38:00 -0800

    added drag and drop functionality to class boxes, able to drag and drop courses from sidebar into quarter, class count and unit count updates correctly

--- src/components/Home/HomeMain/HomeSidebar/ClassCategories/ClassCategories.tsx
+++ src/components/Home/HomeMain/HomeSidebar/ClassCategories/ClassCategories.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import ExpandLessOutlinedIcon from '@mui/icons-material/ExpandLessOutlined';
 import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
-import { Box, Drawer, List, ListItemButton, ListItemText, Collapse } from '@mui/material';
+import { Typography, Box, Drawer, List, ListItemButton, ListItemText, Collapse } from '@mui/material';
+import { Widgets } from '@mui/icons-material';
 
 type Props = {
   sectname: string;
@@ -9,7 +10,14 @@ type Props = {
 
 const ClassCategories = (props: Props) => {
   const [open, setOpen] = useState(false);
+  
 
+  function handleOnDrag(e: React.DragEvent, className: string, classUnits: number) {
+    e.dataTransfer.setData("className", className)
+    e.dataTransfer.setData("classUnits", classUnits.toString())
+  }
+  
+  
   return (
     <>
       <ListItemButton className='mx-4 my-2' onClick={() => setOpen(!open)}>
@@ -17,10 +25,41 @@ const ClassCategories = (props: Props) => {
         {open ? <ExpandLessOutlinedIcon /> : <ExpandMoreOutlinedIcon />}
       </ListItemButton>
       <Collapse in={open}>
-        <List>
-          <p>class selections here</p>
-          <p>create new prop to pass here?</p>
-        </List>
+        {/* <Box
+        className='bg-bgGray'
+        display='flex'
+        flexDirection='column'
+        alignItems='center'
+        border='1px dashed #404040'
+        borderRadius={2}
+        width='100'
+        padding={2}
+        gap={2}
+        onDrop={handleOnDrop}
+        onDragOver={(e) => e.preventDefault()} // Allow drop by preventing the default behavior
+      > */}
+     <Box
+        draggable
+        gap={10}
+        width="90%"
+        maxHeight="100px"
+        onDragStart={(e) => handleOnDrag(e, "cs31", 4)}
+        className="bg-uclaBlue text-bgWhite p-4 rounded-lg cursor-pointer my-2 align-content: center text-center min-h-[50px]"
+      >
+        <Typography variant='body2'>CS31</Typography>
+      </Box>
+      <Box
+        draggable
+        gap={10}
+        maxHeight="100px"
+        width="90%"
+        onDragStart={(e) => handleOnDrag(e, "cs32", 3)}
+        className="bg-uclaBlue text-bgWhite p-4 rounded-lg cursor-pointer text-center min-h-[50px]"
+      >
+        <Typography variant='body2'>CS32</Typography>
+      </Box>
+
+      {/* </Box> */}
       </Collapse>
     </>
   );
