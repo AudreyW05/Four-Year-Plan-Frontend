2944aa24b621d52b665e07d6b73c7e37e026ba54
Brendan Staines <brendanstaines@gmail.com>
2024-11-26 16:22:56 -0800

    Added Dynamic Profile Button

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,10 +3,15 @@ import React from 'react';
 import HomeHeader from '@/components/Home/HomeHeader/HomeHeader';
 import HomeMain from '@/components/Home/HomeMain/HomeMain';
 
+import { useSelector } from 'react-redux';
+import { getCurrentUser } from '@/modules/user/userSlice';
+
 const Home = () => {
+  const currentUser = useSelector(getCurrentUser) ?? { id: -1, email: 'q@gmail.com' };
+
   return (
     <>
-      <HomeHeader></HomeHeader>
+      <HomeHeader currentUser={currentUser} />
       <main>
         <HomeMain></HomeMain>
       </main>
